1dd4d665cd974f2ba67f82c8eb7457b5b459fae0
unknown <tomonishshah@gmail.com>
2026-01-03 20:15:37 +0530

    Star type node for neo4j

---
+++ src/main/java/com/monish/constellation/constellation_graph/domain/node/Star.java
@@ -0,0 +1,34 @@
+package com.monish.constellation.constellation_graph.domain.node;
+
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+
+@Node
+public class Star {
+    @Id
+    private final Long sourceId;
+
+    private final double ra;
+    private final double dec;
+    private final double gmag;
+
+    public Star(Long sourceId, double ra, double dec, double gmag){
+        this.sourceId = sourceId;
+        this.ra = ra;
+        this.dec = dec;
+        this.gmag = gmag;
+    }
+
+    public Long getSourceId() {
+        return sourceId;
+    }
+    public double getRa() {
+        return ra;
+    }
+    public double getDec() {
+        return dec;
+    }
+    public double getGmag() {
+        return gmag;
+    }
+}
