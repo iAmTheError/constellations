52249adf77346c7981f403a6ff72cddbacf90f0e
unknown <tomonishshah@gmail.com>
2026-01-04 19:12:33 +0530

    Ingest controller slice test added

---
+++ src/test/java/com/monish/constellation/constellation_graph/api/controller/IngestControllerTest.java
@@ -0,0 +1,34 @@
+package com.monish.constellation.constellation_graph.api.controller;
+
+import com.monish.constellation.constellation_graph.ingest.gaia.GaiaIngestService;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import org.springframework.test.web.servlet.MockMvc;
+
+import jakarta.annotation.Resource;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(IngestController.class)
+public class IngestControllerTest {
+    @Resource 
+    MockMvc mvc;
+
+    @MockitoBean
+    GaiaIngestService ingestService;
+
+    @Test
+    void ingestEndpointReturnsJson() throws Exception{
+        when(ingestService.ingestBrightestStars(6.0,100)).thenReturn(new GaiaIngestService.IngestResult(150,150));
+    mvc.perform(post("/admin/ingest/bright-stars"))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType("application/json"))
+        .andExpect(jsonPath("$.parsed").value(150))
+        .andExpect(jsonPath("$.saved").value(150));
+    }
+}
---
+++ src/test/java/com/monish/constellation/constellation_graph/ingest/gaia/GaiaIngestServiceTest.java
@@ -0,0 +1,7 @@
+package com.monish.constellation.constellation_graph.ingest.gaia;
+
+import com.monish.constellation.constellation_graph.domain.node.Star;
+
+public class GaiaIngestServiceTest {
+    
+}
