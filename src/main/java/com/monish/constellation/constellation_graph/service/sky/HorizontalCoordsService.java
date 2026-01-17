cd61bd5150050ecd2d3dbe4c372388908db0d314
unknown <tomonishshah@gmail.com>
2026-01-17 15:23:50 +0530

    Convert to Horizontal coords

---
+++ src/main/java/com/monish/constellation/constellation_graph/service/sky/HorizontalCoordsService.java
@@ -0,0 +1,23 @@
+package com.monish.constellation.constellation_graph.service.sky;
+import com.monish.constellation.constellation_graph.service.sky.astronomy.*;
+import org.springframework.stereotype.Service;
+import java.time.Instant;
+
+@Service
+public class HorizontalCoordsService {
+    private final AstronomyFacade astronomyFacade;
+
+    public HorizontalCoordsService(AstronomyFacade astronomyFacade){
+        this.astronomyFacade = astronomyFacade;
+    }
+
+    public AstronomyFacade.Horizontal tHorizontal(
+        double raDeg,
+        double decDeg,
+        double latDeg,
+        double lonDeg,
+        Instant timeUtc
+    ){
+        return astronomyFacade.raDecToAltAz(raDeg, decDeg, latDeg, lonDeg, timeUtc);
+    }
+}
